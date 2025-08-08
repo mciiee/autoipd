38da98379d13a0b8f92b6c594b46161729655601
mciiee <mciiee@klasna.lol>
2025-08-09 02:10:55 +0500

    ip: finished implementation of get_current_ip

--- src/ip.c
+++ src/ip.c
@@ -1,24 +1,47 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <curl/curl.h>
 #include <curl/easy.h>
-#include <stdint.h>
+#include <string.h>
 
 #include "ip.h"
 
 
+static size_t write_to_buffer(char *ptr, size_t size, size_t nmemb, SizedBuffer *userdata){
+    if (nmemb > userdata->size - userdata->occupied) {
+        return 0;
+    }
+    strncpy(userdata->buffer + userdata->occupied, ptr, nmemb);
+    userdata->occupied += nmemb;
+    return nmemb;
+}
 
-char *get_current_ip(char *checkSite){
+char *get_current_ip(const char *checkSite, size_t bufferSize, char buffer[bufferSize]){
     if (checkSite == nullptr) {
         checkSite = DEFAULT_IP_CHECK_SITE;
     }
 
-    auto curl = curl_easy_init(); 
+    SizedBuffer sbuffer = {
+        .size = bufferSize,
+        .occupied = 0,
+        .buffer = buffer
+    };
+    
+    auto curl = curl_easy_init();
 
     curl_easy_setopt(curl, CURLOPT_URL, checkSite);
-    CURLcode result = curl_easy_perform(curl);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_buffer);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &sbuffer);
+
+    CURLcode result = curl_easy_perform(curl);  
     
     if(result !=  CURLE_OK){
         return nullptr;
     }
+    
+    curl_easy_cleanup(curl); 
 
-    return nullptr;
+    return buffer;
 }
--- src/ip.h
+++ src/ip.h
@@ -2,8 +2,15 @@
 #define IP_CHECK_H
 #include <curl/curl.h>
 
-#define DEFAULT_IP_CHECK_SITE "https://icanhazip.com/"
+typedef struct SizedBuffer {
+    size_t size;
+    size_t occupied;
+    char *buffer;
+} SizedBuffer;
 
-char *get_current_ip(char *checkSite);
+constexpr char DEFAULT_IP_CHECK_SITE[] = "https://icanhazip.com/";
 
+
+[[maybe_unused]]
+char *get_current_ip(const char *checkSite, size_t bufferSize, char buffer[bufferSize]);
 #endif
